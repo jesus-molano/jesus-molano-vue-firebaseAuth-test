91eb3dfb2175a099b8fa6db39efc857f3fae5cfc
jesus-molano <jessumolano@gmail.com>
2022-04-16 12:42:51 +0100

    Fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: "jesus-molano-vue-firebaseAuth-test",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
